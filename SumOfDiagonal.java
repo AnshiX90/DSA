3cf6818bbdd083191ef7657ceb9e5aa2ffa5ad34
AnshiX90 <anshika.verma_bca24@gla.ac.in>
2026-02-04 10:05:13 +0530

    Add files via upload

---
+++ SumOfDiagonal.java
@@ -0,0 +1,14 @@
+package p1;
+
+public class SumOfDiagonal {
+
+	public SumOfDiagonal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
